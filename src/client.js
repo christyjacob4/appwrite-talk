0b312f342f644852f9d56a2511ab2e9ae2e2da0a
Christy Jacob <christyjacob4@gmail.com>
2023-03-04 08:50:31 +0530

    feat: create starter template

--- src/client.js
+++ src/client.js
@@ -1,11 +1,2 @@
 import { Account, Client, Databases } from 'appwrite';
 import { APPWRITE_ENDPOINT, APPWRITE_PROJECT_ID } from './constants';
-
-const client = new Client()
-    .setEndpoint(APPWRITE_ENDPOINT)
-    .setProject(APPWRITE_PROJECT_ID);
-
-const account = new Account(client);
-const databases = new Databases(client);
-
-export { client, account, databases };
--- src/service.js
+++ src/service.js
@@ -3,71 +3,22 @@ import { APPWRITE_DATABASE_ID, COLLECTION_MESSAGES, COLLECTION_ROOMS } from "./c
 import { createToastError, createToastSuccess } from "./utils"
 
 export const createAccount = async () => {
-    try {
-        return await account.createAnonymousSession()
-    } catch (error) {
-        createToastError(error.message)
-    }
 }
 
 export const getAccount = async (silent = true) => {
-    try {
-        return await account.get()
-    } catch (error) {
-        !silent && createToastError(error.message)
-    }
 }
 
 export const createRoom = async (roomId, caller) => {
-    try {
-        let res = await databases.createDocument(APPWRITE_DATABASE_ID, COLLECTION_ROOMS, roomId, {
-            caller
-        })
-        createToastSuccess('Room created!')
-        return res;
-    } catch (error) {
-        createToastError(error.message)
-    }
 }
 
 export const updateRoom = async (roomId, caller, callee, silent = true) => {
-    try {
-        let res = await databases.updateDocument(APPWRITE_DATABASE_ID, COLLECTION_ROOMS, roomId, {
-            caller,
-            callee
-        })
-        !silent && createToastSuccess('Room updated successfully')
-        return res;
-    } catch (error) {
-        console.log(error)
-    }
 }
 
 export const getMessages = async (queries) => {
-    try {
-        let results = await databases.listDocuments(
-            APPWRITE_DATABASE_ID,
-            COLLECTION_MESSAGES,
-            queries
-        );
-        return results;
-    } catch (error) {
-        console.log(error)
-    }
 }
 
 export const getRoom = async (roomId) => {
-    try {
-        let res = await databases.getDocument(APPWRITE_DATABASE_ID, COLLECTION_ROOMS, roomId)
-        return res;
-    } catch (error) {
-        console.log(error)
-    }
 }
 
 export const sendToServer = async (message) => {
-    await databases.createDocument(APPWRITE_DATABASE_ID, COLLECTION_MESSAGES, 'unique()', {
-        ...message,
-        payload: JSON.stringify(message.payload)
-    });
 };
